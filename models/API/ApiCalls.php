5479d1bb2574e20de42afcc0376872acbd1a588d
gustavolarsenn <william_gl02@hotmail.com>
2024-09-14 23:06:40 -0300

    Adiciona response no erro em chamadas de API

--- models/API/ApiCalls.php
+++ models/API/ApiCalls.php
@@ -39,20 +39,20 @@ class ApiCalls{
             $response = curl_exec($ch);
     
             if (curl_errno($ch)) {
-                $log->error("Erro ao fazer exceção", ['user' => $_SESSION['id_usuario'], 'page' => isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : $_SERVER['REQUEST_URI'], 'method' => $method, 'url' => $url, 'error' => curl_error($ch)]);
+                $log->error("Erro ao fazer exceção", ['user' => $_SESSION['id_usuario'], 'response' => $response, 'page' => isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : $_SERVER['REQUEST_URI'], 'method' => $method, 'url' => $url, 'error' => curl_error($ch)]);
                 throw new Exception('Erro cURL: ' . curl_error($ch) . ', URL: ' . $url . ', Method: ' . $method);
             }
     
             $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
             if ($httpCode >= 400) {
-                $log->error("Requisição não pôde ser finalizada", ['user' => $_SESSION['id_usuario'], 'page' => isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : $_SERVER['REQUEST_URI'], 'method' => $method, 'url' => $url, 'httpCode' => $httpCode]);
+                $log->error("Requisição não pôde ser finalizada", ['user' => $_SESSION['id_usuario'], 'response' => $response, 'page' => isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : $_SERVER['REQUEST_URI'], 'method' => $method, 'url' => $url, 'httpCode' => $httpCode]);
                 throw new Exception('Erro HTTP: ' . $httpCode . ', URL: ' . $url . ', Method: ' . $method);
             }
     
             curl_close($ch);
     
             if (json_last_error() !== JSON_ERROR_NONE) {
-                $log->error("JSON inválido na resposta da requisição", ['user' => $_SESSION['id_usuario'], 'page' => isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : $_SERVER['REQUEST_URI'], 'method' => $method, 'url' => $url, 'json' => json_last_error_msg()]);
+                $log->error("JSON inválido na resposta da requisição", ['user' => $_SESSION['id_usuario'], 'response' => $response, 'page' => isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : $_SERVER['REQUEST_URI'], 'method' => $method, 'url' => $url, 'json' => json_last_error_msg()]);
                 throw new Exception('JSON inválido na resposta da requisição: ' . json_last_error_msg());
             }
     
